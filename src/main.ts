401e90ce862329845937960ba10d9f412eb05c65
shivvibecodes <shivendrajat3@gmail.com>
2026-05-02 01:53:11 +0530

    CB-01: Added console log and Fallback ports

--- src/main.ts
+++ src/main.ts
@@ -39,8 +39,8 @@ async function bootstrap() {
   app.useGlobalInterceptors(new RequestIdInterceptor());
 
   app.enableShutdownHooks();
-
-  const port = configService.get('PORT', { infer: true }) || 5500;
+  console.log('Collabzz Backend App starting...');
+  const port = configService.get('PORT', { infer: true }) || 5500 || 3000 || 4000;
   await app.listen(port, '0.0.0.0');
 }
 void bootstrap();
